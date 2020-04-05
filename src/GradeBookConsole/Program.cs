d9c9112a435eb7ed8b23cec16c36e545776e3711
vicfer89 <vicfer89@gmail.com>
2020-04-05 14:38:01 +0200

    Version Inicial

---
+++ src/GradeBookConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeBookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                //string Decir = "Se ha introducido: " + args[0];
+                Console.WriteLine($"Se ha introducido {args[0]} en argumentos...");
+            }            
+            else
+                Console.WriteLine("No hay texto a escribir");
+        }
+    }
+}
